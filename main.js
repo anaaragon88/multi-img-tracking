af8c8f2eb2d5f598ba00b3342ca81ef718f9ab84
Ana Aragon <ana.aragon88@gmail.com>
2023-09-07 12:33:05 +0200

    more changes size logoF5

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ document.addEventListener('DOMContentLoaded', () => {
     scene.add(light);
 
     const gltf = await loadGLTF('./assets/logof5.gltf');
-    gltf.scene.scale.set(5, 5, 5);
+    gltf.scene.scale.set(2, 2, 2);
     gltf.scene.position.set(0, -0.4, 0);
 
     const hedy = await loadGLTF('./assets/hedy.gltf');
